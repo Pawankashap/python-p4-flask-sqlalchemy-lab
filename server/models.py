071c31a5079246057c4d450fa3bd94a9d954d387
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-09-02 18:28:18 -0700

    lab complete

--- server/models.py
+++ server/models.py
@@ -11,13 +11,30 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+
+    animals = db.relationship('Animal', backref='zookeeper')
+
+    def __repr__(self):
+        return f'<Animal Owner {self.name}>'    
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(255))
+    open_to_visitors = db.Column(db.Boolean, default=True)
+
+    animals = db.relationship('Animal', backref='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    species = db.Column(db.String(255), nullable=False)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))  
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))  
+
